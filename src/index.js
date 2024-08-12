c2107a2e519b1d2cb7ad4a0a5c47946ef6e8d3cf
surajtgoud <surajtgoud@gmail.com>
2024-08-12 20:41:31 +0530

    added dependencies and setup

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     console.log("DB Connected");
+//   } catch (error) {
+//     console.log("Error", error);
+//   }
+// })();
