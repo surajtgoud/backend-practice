331bc7a1b811645517f2aa862955c5e839d577e7
surajtgoud <surajtgoud@gmail.com>
2024-08-13 13:09:34 +0530

    db connection established

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+  path: "./env",
+});
 
+connectDB();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
 
 // (async () => {
 //   try {
