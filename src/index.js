05ea67c4a1be89ad56346d25c57f186a17c31228
surajtgoud <surajtgoud@gmail.com>
2024-08-14 11:35:05 +0530

    setting up middleware for the project

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
-// require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
-
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./constants";
-
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     console.log("DB Connected");
-//   } catch (error) {
-//     console.log("Error", error);
-//   }
-// })();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection Failed !!!", err);
+  });
